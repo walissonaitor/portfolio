eab5f356d7d6ba4817f21e30e80681f59126e78d
walissonaitor <walissonlopes1810@gmail.com>
2018-06-18 15:03:03 -0300

    portfolio

---
+++ script.js
@@ -0,0 +1,62 @@
+var state=0;
+var pandinha = 0;
+
+$().ready(function(){
+
+	//rola a tela até a parte selecionada na intro
+    $(this).css("color", "#0f7f87");
+	$('.menu').on('click', function(){
+        if (state=0) {
+            state=1;
+        }else{
+            state=0;
+        }
+    });
+    if(state=1){
+    $(".menu").css("color", "#0f7f87");
+
+    }else{  
+    $(".menu").css("color", "#000000");
+
+    }   
+
+$("#feedback").click(function(){
+    alert("Sua mensagem foi enviada. Agradeço desde já.");
+});
+
+
+// $("#panda").hover(function(){
+
+// });
+
+$("#panda").click(function(){
+    $("#panda").animate({ width:'450px',height: '350px'},"slow");
+}); 
+
+
+
+
+
+// $('#panda').mouseenter(function()
+// {       
+   
+//     $(this).animate({width: "150%",},600);
+// });
+
+// $('#panda').mouseleave(function()
+// {       
+//     $(this).animate({width: "100%",},600);
+// });
+
+
+// $(function ()
+// {
+//     $('#panda').mouseover( function ()
+//     {
+//         $(this).width(1000);
+//     });
+// });
+
+
+});
+
